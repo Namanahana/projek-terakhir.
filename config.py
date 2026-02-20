7857d36b1d96d9b67fc0baf654dded8dc1d29f11
Namanahana <raihannanazma@gmail.com>
2026-02-20 19:31:34 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# ===== DISCORD =====
+TOKEN = "your_token"
+PREFIX = "!"
+POLLINATIONS_API_KEY = "your_token"
+REPLICATE_API_TOKEN = "your_token"
+HF_TOKEN = "your_token"
+OPENAI_API_KEY = "your_token"
